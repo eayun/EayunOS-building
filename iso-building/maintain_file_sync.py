12ab65a9e60489954c5b26ba29318bcedd991426
Zhao Chao <zhaochao1984@gmail.com>
2014-12-02 17:17:49 +0800

    iso-building: refine outputs of maintain_file_sync.
    Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>

--- iso-building/maintain_file_sync.py
+++ iso-building/maintain_file_sync.py
@@ -118,6 +118,8 @@ def download(release_num):
                 sys.stdout.write("\r%s%% downloaded, %s" % (current, speed))
                 sys.stdout.flush
 
+            sys.stdout.write("\n")
+            sys.stdout.flush
             return "Done"
 
     ret = _run_download(upstream_url, local_path)
@@ -127,18 +129,22 @@ def download(release_num):
         if footprint:
             upstream_footprint = UPSTREAM_URL + footprint
             local_footprint = DEST_DIR + footprint
+            print "Downloading footprint ", footprint
             ret = _run_download(upstream_footprint, local_footprint)
 
             if not ret:
                 os.unlink(local_path)
             else:
+                print "Checking footprint of %s" % local_path
                 saved_dir = os.getcwd()
                 os.chdir(DEST_DIR)
                 status = commands.getstatusoutput('md5sum -c %s' % local_footprint)[0]
                 if status == 0:
+                    print "The downloaded %s seems fine." % local_path
                     os.chdir(saved_dir)
                     return "Done"
                 else:
+                    print "The downloaded %s does not match the md5sum!" % local_path
                     os.unlink(local_footprint)
                     os.unlink(local_path)
                     os.chdir(saved_dir)
