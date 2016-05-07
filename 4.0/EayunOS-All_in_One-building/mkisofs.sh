ca18f0d21e2bea18049d35f28994d7588290ac7a
walteryang <walteryang47@users.noreply.github.com>
2016-05-07 11:21:39 +0800

    Remove legacy dir
    Signed-off-by: walteryang <walteryang47@users.noreply.github.com>

--- 4.0/EayunOS-All_in_One-building/mkisofs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. ./environ.sh
-
-mkisofs -q -r -R -J -T -no-emul-boot -boot-load-size 4 \
-        -b isolinux/isolinux.bin -boot-info-table \
-        -V $PRODUCT_NAME -A "$PRODUCT_NAME $DATESTRING $ARCH DVD" \
-        -publisher "$PUBLISHER"  -p "$PUBLISHER" \
-        -o $PRODUCT_NAME-$VERSION-$RELEASE-$DATESTRING-$ARCH-DVD.iso ./$ISO_DATA_PATH
